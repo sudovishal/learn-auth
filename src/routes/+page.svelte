d3c2743281304774ae7c694b9b4410355a01f6a7
Vishal Shinde <shinde.vishal1809@gmail.com>
2024-06-23 08:11:48 +0530

    initial setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../app.css";
+</script>
+<h1 class="text-3xl font-bold underline" >Welcome to SvelteKit</h1>
+<p class="">Here is where an awesome login/signup auth will be created. It's gonna be legen...dary</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
